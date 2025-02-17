00878aaec278ffe9b51530bcc39a180dc9b45ac2
Augustus Sroka <rokaawoo@gmail.com>
2025-02-17 20:01:13 +0000

    Refactor evaluation functions and update autogenerated files for lecture 11 examples

--- lecture11-programs-as-data/Examples.hs
+++ lecture11-programs-as-data/Examples.hs
@@ -22,18 +22,34 @@ data Exp = Var Var
   deriving (Show)
 
 -- | Evaluate expressions
-eval :: [(Var, Double)] -> Exp -> Maybe -> Maybe Double
-eval env (Var v)     = case lookup v env of
-                         Nothing -> error $ "Unbound variable: " ++ v
-                         Just n  -> n
-eval _   (Const n)   = n
-eval env (Add e1 e2) = eval env e1 + eval env e2
-eval env (Mul e1 e2) = eval env e1 * eval env e2
-eval env (Div e1 0)  = Nothing 
-eval env (Div e1 e2) = eval env e1 / eval env e2
+-- eval :: [(Var, Double)] -> Exp -> Maybe -> Double
+-- eval env (Var v)     = case lookup v env of
+--                          Nothing -> error $ "Unbound variable: " ++ v
+--                          Just n  -> n
+-- eval _   (Const n)   = Just n
+-- eval env (Neg e1)    = eval env e1 * (-1)
+-- eval env (Add e1 e2) = eval env e1 + eval env e2
+-- eval env (Sub e1 e2) = eval env e1 - eval env e2
+-- eval env (Mul e1 e2) = eval env e1 * eval env e2
+-- eval env (Div e1 e2) = eval env e1 / eval env e2
 
+-- eval 
 
-eval 
+-- | Evaluate expressions (Total)
+eval :: [(Var, Double)] -> Exp -> Maybe Double
+eval env (Var v)     = lookup v env
+eval _   (Const n)   = Just n
+eval env (Add e1 e2) = do
+  n1 <- eval env e1
+  n2 <- eval env e2
+  return (n1 + n2)
+eval env (Mul e1 e2) = do
+  n1 <- eval env e1
+  n2 <- eval env e2
+  return (n1 * n2)
+eval env (Neg e)     = do
+  n <- eval env e
+  return (-n)
 
 -- Compute derivative of an expressions with respect to a variable
 deriv :: Exp -> Var -> Exp
@@ -41,11 +57,12 @@ deriv (Var v) x
   | v == x          = Const 1
   | otherwise       = Const 0
 deriv (Const _) _   = Const 0
+deriv (Neg e1) x    = Neg (deriv e1 x)
 deriv (Add e1 e2) x = Add (deriv e1 x) (deriv e2 x)
 deriv (Mul e1 e2) x = Add (Mul e1 (deriv e2 x))
                           (Mul (deriv e1 x) e2)
 
 -- | "Compile" an expression into a Haskell function
-compile :: Exp -> Double -> Double
+compile :: Exp -> Double -> Maybe Double
 compile e x = eval [("x", x)] e
 
